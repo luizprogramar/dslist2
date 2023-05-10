0b36e36f1e0567c37f67a09076744c6bc88c6ac8
luiz henrique <luizaaaljk2009@gmail.com>
2023-05-10 12:49:02 -0300

    projeto criado

---
+++ src/main/java/com/devesuperior/dlist2/Dlist2Application.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dlist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dlist2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dlist2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devesuperior/dlist2/Dlist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dlist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dlist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
